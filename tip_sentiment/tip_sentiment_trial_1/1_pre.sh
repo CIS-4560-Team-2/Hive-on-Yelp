6413ff9e4c50574a46339518ec7474795b8a397a
Randall Lunetto <rlunetto@gmail.com>
2020-10-11 01:39:02 -0700

    Update 1_pre.sh

--- tip_sentiment/tip_sentiment_trial_1/1_pre.sh
+++ tip_sentiment/tip_sentiment_trial_1/1_pre.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-#Creating tip_sentiment directory within the yelp/results directory in Local filesystem
+#Creating tip_sentiment directory within user/malam/yelp/results directory in Local filesystem
 mkdir yelp/results/tip_sentiment
 
-#Creating tip_sentiment_trial directory within the yelp/results/tip_sentiment directory in Local filesystem
+#Creating tip_sentiment_trial directory within the user/malam/yelp/results/tip_sentiment directory in Local filesystem
 mkdir yelp/tip_sentiment_trial_1
 
 #Download dictionary file to analyze sentiment of data
