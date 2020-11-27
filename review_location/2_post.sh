ffe1b91ecedff9d41729ca8e9f28d219be7a30ef
mohsenualam <mohsenualam@gmail.com>
2020-11-27 10:32:35 -0800

    Delete 2_post.sh

--- review_location/2_post.sh
+++
@@ -1,9 +0,0 @@
-mkdir yelp/results/review_location_average
-cd yelp/results/review_location_average
-
-hdfs dfs -get yelp/results/review_location_average/0*
-
-cat 00* > review_location_average.csv
-
-
-scp malam@129.150.79.19:/home/malam/yelp/results/review_location_average/review_location_average.csv review_location_average.csv
