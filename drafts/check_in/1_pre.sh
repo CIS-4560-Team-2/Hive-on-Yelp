d35d761e6583d9e9ee45caff38cb3dd9b8d62afd
mohsenualam <mohsenualam@gmail.com>
2020-11-27 10:43:25 -0800

    Create 1_pre.sh

---
+++ drafts/check_in/1_pre.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#Creating yelp and yelp/results/checkin_count/ in Local filesystem
+mkdir yelp
+mkdir yelp/results
+mkdir yelp/results/checkin_per_day
+
+#Creating checkin_month_year_count/ in Local filesystem
+#mkdir yelp/results/checkin_month_year_total
