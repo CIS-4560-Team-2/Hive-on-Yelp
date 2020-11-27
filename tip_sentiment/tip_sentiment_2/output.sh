1c32564dbf28fdc4ea38f50d7f5d5f55c0857660
mohsenualam <mohsenualam@gmail.com>
2020-11-27 10:32:50 -0800

    Delete output.sh

--- tip_sentiment/tip_sentiment_2/output.sh
+++
@@ -1,9 +0,0 @@
-cd yelp/results/tip_sentiment/
-
-hdfs dfs -get /user/malam/yelp/results/tip_sentiment2
-
-cd yelp/results/tip_sentiment/tip_sentiment2
-
-cat 00* > tip_sentiment2.csv
-
-scp malam@129.150.79.19:/home/malam/yelp/results/tip_sentiment/tip_sentiment2/tip_sentiment2.csv ~/
